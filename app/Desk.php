8b3e970bf2ea61f7bd5008836871d694b60bb075
serii <seriiburduja@gmail.com>
2021-11-23 13:39:03 +0200

    update

--- app/Desk.php
+++ app/Desk.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Desk extends Model
 {
+    protected $fillable = ['name'];
     public function lists()
     {
         return $this->hasMany(DeskList::class);
--- app/Http/Controllers/Api/DeskController.php
+++ app/Http/Controllers/Api/DeskController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Desk;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\DeskStoreRequest;
 use App\Http\Resources\DeskResource;
 use Illuminate\Http\Request;
 
@@ -12,7 +13,6 @@ class DeskController extends Controller
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
      */
     public function index()
     {
@@ -23,11 +23,12 @@ class DeskController extends Controller
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(DeskStoreRequest $request)
     {
-        //
+        // user validate to use just fields from $fillable from Desk model.
+        $created_desk = Desk::create($request->validated());
+        return new DeskResource($created_desk);
     }
 
     /**
@@ -45,11 +46,11 @@ class DeskController extends Controller
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  int  $id
-     * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(DeskStoreRequest $request, Desk $desk)
     {
-        //
+        $desk->update($request->validated());
+        return new DeskResource($desk);
     }
 
     /**
