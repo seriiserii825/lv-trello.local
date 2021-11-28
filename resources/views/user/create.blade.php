bf85f9c72b5d3273f8e2a12a538dafef82483771
serii <seriiburduja@gmail.com>
2021-11-28 09:29:41 +0200

    debug

--- resources/views/user/create.blade.php
+++ resources/views/user/create.blade.php
@@ -1,10 +1,12 @@
 @extends('layouts.user-layout')
 @section('content')
     <h2 class="user-layout__title">Registration</h2>
-    <form action="{{ route('user.store') }}" method="post">
+    <form class="form" action="{{ route('user.store') }}" method="post">
         @csrf
-        <input class="@error('name') is-invalid @enderror" type="text" name="name" value="{{ old('name') }}"  placeholder="Name">
-        <input class="@error('email') is-invalid @enderror" type="email" name="email" value="{{ old('email') }}"  placeholder="Email">
+        <input class="@error('name') is-invalid @enderror" type="text" name="name" value="{{ old('name') }}"
+            placeholder="Name">
+        <input class="@error('email') is-invalid @enderror" type="email" name="email" value="{{ old('email') }}"
+            placeholder="Email">
         <input class="@error('password') is-invalid @enderror" type="password" name="password" placeholder="Password">
         <input type="password" name="password_confirmed" placeholder="Confirm password">
         <p class="info">Have an account, <a href="{{ route('user.login') }}">Login</a></p>
--- resources/views/user/login.blade.php
+++ resources/views/user/login.blade.php
@@ -1,10 +1,15 @@
 @extends('layouts.user-layout')
 @section('content')
     <h2 class="user-layout__title">Login</h2>
-    <form action="{{ route('user.login') }}" method="post">
+    <form class="form" action="{{ route('user.login') }}" method="post">
         @csrf
-        <input class="@error('email') is-invalid @enderror" type="email" name="email" value="{{ old('email') }}" placeholder="Email">
-        <input class="@error('password') is-invalid @enderror" type="password" name="password" placeholder="Password">
+        <div class="form-group">
+            <input class="@error('email') is-invalid @enderror" type="email" name="email" value="{{ old('email') }}"
+                placeholder="Email">
+        </div>
+        <div class="form-group">
+            <input class="@error('password') is-invalid @enderror" type="password" name="password" placeholder="Password">
+        </div>
         <p class="info">Don't have an account, <a href="{{ route('user.create') }}">Register</a></p>
         <button class="btn" type="submit">Submit</button>
     </form>
