8027f89ce65004064ca7643c8c99a33254cb0e59
serii <seriiburduja@gmail.com>
2021-11-30 09:27:34 +0200

    project table styled

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -1,16 +1,18 @@
 @extends('layouts.front-layout')
 @section('content')
-    <div class="project__wrap">
-        @if (count($projects_list))
-            @foreach ($projects_list as $project)
-                <a href="{{ route('project.single', ['slug' => $project->slug]) }}" class="project"
-                    style="background: {{ $project->color }}">
-                    <h3 class="project__title">{{ $project->title }}</h3>
-                    <div class="project__tasks">3</div>
-                </a>
-            @endforeach
-        @else
-            <h3>No projects yet...</h3>
-        @endif
+    <div class="container">
+        <div class="project__wrap">
+            @if (count($projects_list))
+                @foreach ($projects_list as $project)
+                    <a href="{{ route('project.single', ['slug' => $project->slug]) }}" class="project"
+                        style="background: {{ $project->color }}">
+                        <h3 class="project__title">{{ $project->title }}</h3>
+                        <div class="project__tasks">3</div>
+                    </a>
+                @endforeach
+            @else
+                <h3>No projects yet...</h3>
+            @endif
+        </div>
     </div>
 @endsection
--- resources/views/projects/single.blade.php
+++ resources/views/projects/single.blade.php
@@ -3,127 +3,139 @@
     <div class="single-project" style="background: {{ $project->color }}">
         <div class="container">
             <h2 class="single-project__title">{{ $project->title }}</h2>
-            @if(count($cards))
-                <table class="project-table">
-                    <thead>
-                    <th>Wait to do</th>
-                    <th>To do</th>
-                    <th>In progress</th>
-                    <th>Done</th>
-                    <th>Tested</th>
-                    </thead>
-                    <tbody>
-                    @foreach($cards as $card)
-                        <tr>
-                            <td>
-                                @if($card->column_id === 1)
+            @if (count($cards))
+                <div class="project-table">
+                    <div class="project-table__column">
+                        <h3 class="project-table__title">Wait to do</h3>
+                        @foreach ($cards as $card)
+                            @if ($card->column_id === 1)
+                                <div class="project-table__row">
                                     <div class="card">
-                                        <h3>{{ $card->title }}</h3>
-                                        <p>{{ $card->created_at }}</p>
+                                        <h2>{{ $card->title }}</h2>
                                         <form action="{{ route('cards.update', ['card' => $card->id]) }}" method="post">
                                             @csrf
                                             @method('PUT')
                                             <div class="form-group">
                                                 <select name="column_id" id="column_id">
-                                                    @foreach($columns as $column)
-                                                        <option @if($column->id === $card->column_id) selected
-                                                                @endif value="{{ $column->id }}">{{ $column->title }}</option>
+                                                    @foreach ($columns as $column)
+                                                        <option @if ($column->id === $card->column_id) selected
+                                                            @endif value="{{ $column->id }}">{{ $column->title }}
+                                                        </option>
                                                     @endforeach
                                                 </select>
                                             </div>
                                             <input type="submit" class="btn" value="Update">
                                         </form>
                                     </div>
-                                @endif
-                            </td>
-                            <td>
-                                @if($card->column_id === 2)
+                                </div>
+                            @endif
+                        @endforeach
+                    </div>
+                    <div class="project-table__column">
+                        <h3 class="project-table__title">To do</h3>
+                        @foreach ($cards as $card)
+                            @if ($card->column_id === 2)
+                                <div class="project-table__row">
                                     <div class="card">
-                                        <h3>{{ $card->title }}</h3>
-                                        <p>{{ $card->created_at }}</p>
+                                        <h2>{{ $card->title }}</h2>
                                         <form action="{{ route('cards.update', ['card' => $card->id]) }}" method="post">
                                             @csrf
                                             @method('PUT')
                                             <div class="form-group">
                                                 <select name="column_id" id="column_id">
-                                                    @foreach($columns as $column)
-                                                        <option @if($column->id === $card->column_id) selected
-                                                                @endif value="{{ $column->id }}">{{ $column->title }}</option>
+                                                    @foreach ($columns as $column)
+                                                        <option @if ($column->id === $card->column_id) selected
+                                                            @endif value="{{ $column->id }}">{{ $column->title }}
+                                                        </option>
                                                     @endforeach
                                                 </select>
                                             </div>
                                             <input type="submit" class="btn" value="Update">
                                         </form>
                                     </div>
-                                @endif
-                            </td>
-                            <td>
-                                @if($card->column_id === 3)
+                                </div>
+                            @endif
+                        @endforeach
+                    </div>
+                    <div class="project-table__column">
+                        <h3 class="project-table__title">In progress</h3>
+                        @foreach ($cards as $card)
+                            @if ($card->column_id === 3)
+                                <div class="project-table__row">
                                     <div class="card">
-                                        <h3>{{ $card->title }}</h3>
-                                        <p>{{ $card->created_at }}</p>
+                                        <h2>{{ $card->title }}</h2>
                                         <form action="{{ route('cards.update', ['card' => $card->id]) }}" method="post">
                                             @csrf
                                             @method('PUT')
                                             <div class="form-group">
                                                 <select name="column_id" id="column_id">
-                                                    @foreach($columns as $column)
-                                                        <option @if($column->id === $card->column_id) selected
-                                                                @endif value="{{ $column->id }}">{{ $column->title }}</option>
+                                                    @foreach ($columns as $column)
+                                                        <option @if ($column->id === $card->column_id) selected
+                                                            @endif value="{{ $column->id }}">{{ $column->title }}
+                                                        </option>
                                                     @endforeach
                                                 </select>
                                             </div>
                                             <input type="submit" class="btn" value="Update">
                                         </form>
                                     </div>
-                                @endif
-                            </td>
-                            <td>
-                                @if($card->column_id === 4)
+                                </div>
+                            @endif
+                        @endforeach
+                    </div>
+                    <div class="project-table__column">
+                        <h3 class="project-table__title">Done</h3>
+                        @foreach ($cards as $card)
+                            @if ($card->column_id === 4)
+                                <div class="project-table__row">
                                     <div class="card">
-                                        <h3>{{ $card->title }}</h3>
-                                        <p>{{ $card->created_at }}</p>
+                                        <h2>{{ $card->title }}</h2>
                                         <form action="{{ route('cards.update', ['card' => $card->id]) }}" method="post">
                                             @csrf
                                             @method('PUT')
                                             <div class="form-group">
                                                 <select name="column_id" id="column_id">
-                                                    @foreach($columns as $column)
-                                                        <option @if($column->id === $card->column_id) selected
-                                                                @endif value="{{ $column->id }}">{{ $column->title }}</option>
+                                                    @foreach ($columns as $column)
+                                                        <option @if ($column->id === $card->column_id) selected
+                                                            @endif value="{{ $column->id }}">{{ $column->title }}
+                                                        </option>
                                                     @endforeach
                                                 </select>
                                             </div>
                                             <input type="submit" class="btn" value="Update">
                                         </form>
                                     </div>
-                                @endif
-                            </td>
-                            <td>
-                                @if($card->column_id === 5)
+                                </div>
+                            @endif
+                        @endforeach
+                    </div>
+                    <div class="project-table__column">
+                        <h3 class="project-table__title">Test</h3>
+                        @foreach ($cards as $card)
+                            @if ($card->column_id === 5)
+                                <div class="project-table__row">
                                     <div class="card">
-                                        <h3>{{ $card->title }}</h3>
-                                        <p>{{ $card->created_at }}</p>
+                                        <h2>{{ $card->title }}</h2>
                                         <form action="{{ route('cards.update', ['card' => $card->id]) }}" method="post">
                                             @csrf
                                             @method('PUT')
                                             <div class="form-group">
                                                 <select name="column_id" id="column_id">
-                                                    @foreach($columns as $column)
-                                                        <option @if($column->id === $card->column_id) selected
-                                                                @endif value="{{ $column->id }}">{{ $column->title }}</option>
+                                                    @foreach ($columns as $column)
+                                                        <option @if ($column->id === $card->column_id) selected
+                                                            @endif value="{{ $column->id }}">{{ $column->title }}
+                                                        </option>
                                                     @endforeach
                                                 </select>
                                             </div>
                                             <input type="submit" class="btn" value="Update">
                                         </form>
                                     </div>
-                                @endif
-                            </td>
-                        </tr>
-                    @endforeach
-                    </tbody>
-                </table>
+                                </div>
+                            @endif
+                        @endforeach
+                    </div>
+                </div>
             @else
                 <h2>No cards</h2>
             @endif
